da0bf1ce9440d726702ca73c4df87aa0a7d23a62
Ibrahim Aboubacar Cisse <ibrahimaboubacarcisee@gmail.com>
2023-12-18 05:21:30 +0000

    Creation of Gallery and thégallery system, active page implementation, new images added, fixe text writer

--- includes/constants.php
+++ includes/constants.php
@@ -8,8 +8,8 @@ define("IMG_PATH", "assets/img/");
 /**
  * The URL of the website.
  */
-// define("SITE_BASE", "https://future-generation.test/");
-define("SITE_BASE", "http://192.168.100.3/future-generation/public_html/");
+define("SITE_BASE", "https://future-generation.test/");
+// define("SITE_BASE", "http://192.168.100.3/future-generation/public_html/");
 
 /**
  * An array listing the pages within the website.
